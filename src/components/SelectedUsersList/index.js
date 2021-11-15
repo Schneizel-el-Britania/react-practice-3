4a3cc3c2f7eb710fb77edd21b93ed99720035432
Schneizel-el-Britania <kmmaxim1@gmail.com>
2021-11-15 19:27:21 +0200

    Add ListPage component

--- src/components/SelectedUsersList/index.js
+++ src/components/SelectedUsersList/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
 const SelectedUsersList = (props) => {
-  const {users} = props;
-  const selectedUsers = users.filter((user)=>user.isSelected);
+  const { users } = props;
+  const selectedUsers = users.filter((user) => user.isSelected);
   return (
     <div>
-      {selectedUsers.map(({id, fname})=>{
-        return <span key={id}>{fname}</span>
+      {selectedUsers.map(({ id, fname }) => {
+        return <div key={id}>{fname}</div>
       })}
     </div>
   );
